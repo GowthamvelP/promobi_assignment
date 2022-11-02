9a3db6c986e34d8ffe54df14e0e6aaad786eb05c
Gowthamvel Palanivel <gowthamvelpalanivel@gmail.com>
2022-11-02 07:18:16 +0530

    Added unit test cases for the tutors list

--- spec/controllers/api/v1/courses_controller_spec.rb
+++ spec/controllers/api/v1/courses_controller_spec.rb
@@ -1,6 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CoursesController, type: :controller do
+  describe "#index" do
+    let(:course) { FactoryBot.create(:course, title: "1st Course", description: "This is my first course!") }
+    let(:tutor1) { FactoryBot.create(:tutor, name: "Tutor 1", course: course) }
+    let(:tutor2) { FactoryBot.create(:tutor, name: "Tutor 2", course: course) }
+
+    context "when valid params are passed" do
+      let(:params) do
+        {
+          page: 1,
+          per_page: 20
+        }
+      end
+
+      it 'should list courses with associated tutors' do
+        get :index, params: params
+        response_json = JSON.parse(response.body)
+        expect(response_json['data'][0]['title'].to_s).to eql(course.title.to_s)
+        expect(response_json['data'][0]['description'].to_s).to eq(course.description.to_s)
+        expect(response_json['data'][0]['tutors'][0]['name']).to eq(tutor1.name)
+        expect(response_json['metadata']['page']).to eq(params[:page])
+        expect(response_json['metadata']['per_page']).to eq(params[:per_page])
+        expect(response_json['metadata']['total_entries']).to eq(1)
+      end
+    end
+
+    context "when invalid page params are passed" do
+      let(:params) do
+        {
+          page: 'hi',
+          per_page: 20
+        }
+      end
+
+      it 'should respond with an invalid page error' do
+        get :index, params: params
+        response_json = JSON.parse(response.body)
+        expect(response_json['success']).to eq(false)
+        expect(response_json['message']).to eq("Invalid page.")
+      end
+    end
+
+    context "when invalid page params are passed" do
+      let(:params) do
+        {
+          page: 1,
+          per_page: "hey there!"
+        }
+      end
+
+      it 'should respond with an invalid per_page error' do
+        get :index, params: params
+        response_json = JSON.parse(response.body)
+        expect(response_json['success']).to eq(false)
+        expect(response_json['message']).to eq("Invalid per page.")
+      end
+    end
+  end
 
   describe "#create" do
     before do
@@ -82,34 +139,6 @@ RSpec.describe Api::V1::CoursesController, type: :controller do
           response_json = JSON.parse(response.body)
           expect(response_json['message']).to eq(["Tutors name can't be blank"])
         end
-      end
-    end
-  end
-
-  describe "#index" do
-    let(:course) { FactoryBot.create(:course, title: "1st Course", description: "This is my first course!") }
-    let(:tutor1) { FactoryBot.create(:tutor, name: "Tutor 1", course: course) }
-    let(:tutor2) { FactoryBot.create(:tutor, name: "Tutor 2", course: course) }
-
-    context "when valid" do
-      let(:params) do
-        {
-          page: 1,
-          per_page: 20
-        }
-      end
-
-      it 'should list courses' do
-        puts course, tutor1, tutor2
-        get :index, params: params
-        response_json = JSON.parse(response.body)
-        puts response_json
-        expect(response_json['data'][0]['title']).to eq(course.title)
-        expect(response_json['data'][0]['description']).to eq(course.description)
-        expect(response_json['data'][0]['tutors']['name']).to eq(tutors.first.name)
-        expect(response_json['metadata']['page']).to eq(params[:page])
-        expect(response_json['metadata']['per_page']).to eq(params[:per_page])
-        expect(response_json['metadata']['total_entries']).to eq(1)
       end
     end
   end
